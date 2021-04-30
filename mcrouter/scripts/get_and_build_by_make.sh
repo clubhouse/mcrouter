a7f2c037193265b06b0c6e6e141a418fdcbd0494
Paul Groudas <paul@clubhouse.io>
2021-04-30 11:53:46 -0400

    Fail on systems with insufficient memory.

--- mcrouter/scripts/get_and_build_by_make.sh
+++ mcrouter/scripts/get_and_build_by_make.sh
@@ -13,6 +13,13 @@ INSTALL_AUX_DIR="${1%/}"/install/aux
 
 [ -n "$TARGET" ] || ( echo "Target missing"; exit 1 )
 
+SYSTEM_MEM="$(free -g | grep "^Mem:" | tr -s '[:blank:]' | cut -d ' ' -f 2)"
+if (( ${SYSTEM_MEM:-0} <= 25 )); then
+    echo "mcrouter needs a lot of memory to compile, and is known to exhaust memory on 16gb systems." >&2
+    echo "On EC2 consider an instance with 32 GB of memory, like an m6g.2xlarge or m5a.2xlarge" >&2
+    exit 1
+fi
+
 mkdir -p "$PKG_DIR" "$INSTALL_DIR" "$INSTALL_AUX_DIR"
 mkdir -p "$INSTALL_DIR/lib"
 
